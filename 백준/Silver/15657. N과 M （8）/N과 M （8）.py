f2a9231a706cb5d39137f673a9487377502abeba
sminju1009 <139519077+sminju1009@users.noreply.github.com>
2023-09-30 18:05:21 +0900

    [Silver III] Title: N과 M (8), Time: 156 ms, Memory: 115840 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,21 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+path = []
+
+def abc(level):
+    if level == m:
+        print(' '.join(map(str, path)))
+        return
+    for i in range(n):
+        if level==0:
+            path.append(arr[i])
+            abc(level + 1)
+            path.pop()
+        else:
+            if path[level-1]<=arr[i]:
+                path.append(arr[i])
+                abc(level + 1)
+                path.pop()
+
+abc(0)
