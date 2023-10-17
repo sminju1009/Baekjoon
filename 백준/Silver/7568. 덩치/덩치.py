277fdc3b961ece4afd965f6f0e17dd43c238395d
sminju1009 <139519077+sminju1009@users.noreply.github.com>
2023-10-17 16:28:03 +0900

    [Silver V] Title: 덩치, Time: 116 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,11 @@
+n = int(input())
+arr = []
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+for i in range(n):
+    cnt = 0
+    for j in range(n):
+        if i!=j and arr[i][0]<arr[j][0] and arr[i][1]<arr[j][1]:
+            cnt += 1
+    print(cnt+1, end=" ")
