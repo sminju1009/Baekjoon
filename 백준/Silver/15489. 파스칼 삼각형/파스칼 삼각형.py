59315b939fb7d24d3c25c9b56a75cc3b4da8d074
서민주 <139519077+sminju1009@users.noreply.github.com>
2024-01-02 13:42:06 +0900

    [Silver IV] Title: 파스칼 삼각형, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/15489. 파스칼 삼각형/파스칼 삼각형.py
@@ -0,0 +1,13 @@
+r, c, w = map(int, input().split())
+dp = [[1]*i for i in range(1, 32)]
+
+for i in range(2, 31):
+    for j in range(1, len(dp[i])-1):
+        dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+        
+cnt = 0
+for i in range(r-1, r+w-1):
+    for j in range(c-1, c+(i-r)+1):
+        cnt += dp[i][j]
+        
+print(cnt)
