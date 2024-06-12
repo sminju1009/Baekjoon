5a2ebe3e06e45e8e5fb25ab8c8219c2b5d3da95a
서민주 <sminju1009@gmail.com>
2024-06-12 09:23:02 +0900

    [Gold IV] Title: 전력난, Time: 2288 ms, Memory: 214560 KB -BaekjoonHub

---
+++ 백준/Gold/6497. 전력난/전력난.py
@@ -0,0 +1,53 @@
+# 특정 원소가 속한 집합을 찾기
+def find_parent(parent, x):
+    # 루트 노드를 찾을 때까지 재귀 호출
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+# 두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a<b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+while True:
+    # 노드의 개수와 간선의 개수 입력받기
+    v, e = map(int, input().split())
+    if v==0 and e==0:
+        break
+    parent = [0] * (v+1)
+
+    # 모든 간선을 담을 리스트와, 최종 비용을 담을 변수
+    edges = []
+    result = 0
+
+    # 부모 테이블상에서, 부모를 자기 자신으로 초기화
+    for i in range(1, v+1):
+        parent[i] = i
+
+    # 제외되기 전 전체 비용을 담을 변수
+    answer = 0
+
+    # 모든 간선에 대한 정보 입력받기
+    for _ in range(e):
+        a, b, cost = map(int, input().split())
+        # 비용순으로 정렬하기 위해 튜플의 첫 원소를 비용으로 설정
+        edges.append((cost, a, b))
+        answer += cost
+
+    # 간선을 비용순으로 설정
+    edges.sort()
+
+    # 간선을 하나씩 확인하며
+    for edge in edges:
+        cost, a, b = edge
+        # 사이클이 발생하지 않는 경우에만 집합에 포함
+        if find_parent(parent, a) != find_parent(parent, b):
+            union_parent(parent, a, b)
+            result += cost
+
+    print(answer-result)
