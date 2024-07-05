5b38b31785ab577c38bbd9d6e91ae18b47061dd0
서민주 <sminju1009@gmail.com>
2024-07-05 11:02:23 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 60 ms, Memory: 31132 KB -BaekjoonHub

---
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+def dfs(y, x, color):
+    global cnt
+    visited[y][x] = 1
+    cnt += 1
+    for dy, dx in ((0, -1), (-1, 0), (0, 1), (1, 0)):
+        ny, nx = dy + y, dx + x
+        if 0 <= ny < n and 0 <= nx < m and arr[ny][nx] == color and visited[ny][nx] == 0:
+            dfs(ny, nx, color)
+
+m, n = map(int, input().split())
+arr = [list(input().strip()) for _ in range(n)]
+visited = [[0] * m for _ in range(n)]
+result_w = []
+result_b = []
+
+# W = 우리팀 병사 / B = 적국 병사
+for i in range(n):
+    for j in range(m):
+        if arr[i][j] == 'W' and visited[i][j] == 0:
+            cnt = 0
+            dfs(i, j, 'W')
+            result_w.append(cnt ** 2)
+        elif arr[i][j] == 'B' and visited[i][j] == 0:
+            cnt = 0
+            dfs(i, j, 'B')
+            result_b.append(cnt ** 2)
+
+print(sum(result_w), sum(result_b))
