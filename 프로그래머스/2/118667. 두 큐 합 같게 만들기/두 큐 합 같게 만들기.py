7788e8e9f261e2933bf0147f484499049d514d80
서민주 <sminju1009@gmail.com>
2024-07-29 21:12:03 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 163.46 ms, Memory: 19.2 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -3,7 +3,6 @@ from collections import deque
 def function(queue1, queue2):
     cnt = 0 # 작업한 횟수
     sum1, sum2 = sum(queue1), sum(queue2)
-    total_operations = 0
     
     # deque로 변환하여 pop(0)의 시간 복잡도를 줄임
     queue1 = deque(queue1)
@@ -11,7 +10,7 @@ def function(queue1, queue2):
     
     while cnt < 2 * (len(queue1) + len(queue2)):  # 최대 작업 횟수 제한
         if sum1 == sum2:
-            return total_operations
+            return cnt
         
         if sum1 > sum2:
             if queue1:
@@ -27,7 +26,6 @@ def function(queue1, queue2):
                 sum1 += a
         
         cnt += 1
-        total_operations += 1
     
     return -1
 
