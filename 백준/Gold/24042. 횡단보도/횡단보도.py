40d6f69478bb0086124192e87141e3fe5c8280aa
서민주 <sminju1009@gmail.com>
2024-07-31 10:38:28 +0900

    [Gold I] Title: 횡단보도, Time: 1480 ms, Memory: 227768 KB -BaekjoonHub

--- 백준/Gold/24042. 횡단보도/횡단보도.py
+++ 백준/Gold/24042. 횡단보도/횡단보도.py
@@ -1,30 +1,29 @@
 import heapq
 import sys
 input = sys.stdin.readline
-N, M = map(int, input().split())
-arr = [[] for _ in range(N+1)]
-for i in range(M):
+n, m = map(int, input().split())
+visited = [sys.maxsize for _ in range(n+1)]
+arr = [[] for _ in range(n+1)]
+for i in range(m):
     a, b = map(int, input().split())
     arr[a].append((i, b))
     arr[b].append((i, a))
 
-
-def dijkstra():
+def dijkstra(start):
     q = []
-    heapq.heappush(q, (0, 1))
-    visited = [sys.maxsize for _ in range(N+1)]
-    visited[1] = 0
+    heapq.heappush(q, (0, start))
+    visited[start] = 0
     while q:
         time, node = heapq.heappop(q)
-        if node == N:
-            return time
+        if node == n:
+            return
         if visited[node] < time:
             continue
         for i, nnode in arr[node]:
-            ntime = i+((time-i)//M)*M if (time-i)%M==0 else i+((time-i)//M+1)*M
+            ntime = i+((time-i)//m)*m if (time-i)%m==0 else i+((time-i)//m+1)*m
             if visited[nnode] > ntime+1:
                 visited[nnode] = ntime+1
                 heapq.heappush(q, (ntime+1, nnode))
 
-
-print(dijkstra())
+dijkstra(1)
+print(visited[-1])
