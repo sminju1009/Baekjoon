336ce9b051c883f46b40d09754655b077a8e54d1
서민주 <sminju1009@gmail.com>
2024-08-10 12:37:30 +0900

    [Silver III] Title: N과 M (6), Time: 124 ms, Memory: 15904 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int n, m;
+    static int[] result;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        result = new int[m];
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int level){
+        if (level==m) {
+            for (int i = 0; i<m; i++) {
+                sb.append(result[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i<n; i++) {
+            int max = Arrays.stream(result).max().getAsInt();
+            if (max<arr[i]) {
+                result[level] = arr[i];
+                dfs(level+1);
+                result[level] = 0;
+            }
+        }
+    }
+}
