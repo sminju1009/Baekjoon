42dd81922eb60f42cb374389f072386513bba852
서민주 <sminju1009@gmail.com>
2024-08-31 12:08:18 +0900

    [level 4] Title: 도둑질, Time: 577.59 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.py
+++ 프로그래머스/4/42897. 도둑질/도둑질.py
@@ -1,23 +1,23 @@
 def solution(money):
     n = len(money)
-    
+
     if n == 1:
         return money[0]
-    
+
     # 첫 번째 집을 터는 경우 (마지막 집은 안 턴다)
     dp1 = [0] * n
     dp1[0] = money[0]
     dp1[1] = max(money[0], money[1])
-    
+
     for i in range(2, n-1):  # n-1번째 집까지 가능
         dp1[i] = max(dp1[i-1], dp1[i-2] + money[i])
-    
+
     # 첫 번째 집을 털지 않는 경우 (마지막 집을 털 수 있음)
     dp2 = [0] * n
     dp2[1] = money[1]
-    
+
     for i in range(2, n):
         dp2[i] = max(dp2[i-1], dp2[i-2] + money[i])
-    
+
     # 두 경우 중 큰 값을 반환
     return max(dp1[-2], dp2[-1])
