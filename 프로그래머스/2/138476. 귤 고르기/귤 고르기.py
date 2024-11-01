2278e112890b8db7dd5b271de02ab68ee77dadfc
서민주 <sminju1009@gmail.com>
2024-11-01 21:38:16 +0900

    [level 2] Title: 귤 고르기, Time: 99.62 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,15 +1,14 @@
+from collections import defaultdict
+
 def solution(k, tangerine):
     answer = 0
-    box = dict()
+    box = defaultdict(int)
     for i in tangerine:
-        if i in box:
-            box[i] += 1
-        else:
-            box[i] = 1
+        box[i] += 1
     
     # 내림차순 정렬하기
     box_sort = dict(sorted(box.items(), key= lambda item:item[1], reverse=True))
-    
+        
     for i in box_sort:
         
         k -= box_sort[i]
