6da715601c9acd6e67512f49959cde61180f1a91
서민주 <sminju1009@gmail.com>
2025-03-04 10:44:02 +0900

    [Silver III] Title: 퇴사, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -2,37 +2,29 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
-    static int[][] arr;
-    static int n;
-    static int result = 0;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n][2];
-        for (int i = 0; i < n; i++) {
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n+2][2];
+        int[] dp = new int[n+2];
+        for (int i = 1; i<n+1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());  // 상담 기간
-            arr[i][1] = Integer.parseInt(st.nextToken());  // 상담 금액
-        }
-        backtracking(0, 0);
-        System.out.println(result);
-    }
-
-    public static void backtracking(int day, int sum) {
-        // 기저 조건: 모든 날을 다 돌았을 경우
-        if (day == n) {
-            result = Math.max(result, sum); // 최종 결과 갱신
-            return;
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        // 오늘 일을 선택할 수 있는 경우
-        if (day + arr[day][0] <= n) {
-            backtracking(day + arr[day][0], sum + arr[day][1]);
+        int max = -1;
+        for (int i = 1; i<=n+1; i++) {
+            if (max<dp[i]) max = dp[i];
+            int next = i+arr[i][0];
+            int cost = arr[i][1];
+            if (next < n+2) {
+                dp[next] = Math.max(dp[next], max+cost);
+            }
         }
 
-        // 오늘 일을 선택하지 않는 경우
-        backtracking(day + 1, sum);
+        System.out.println(dp[n+1]);
+
     }
 }
