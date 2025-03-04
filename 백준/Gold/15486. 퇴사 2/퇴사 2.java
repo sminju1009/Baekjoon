410b96aa7c235d725f55c69a22692dbca0224015
서민주 <sminju1009@gmail.com>
2025-03-04 10:32:06 +0900

    [Gold V] Title: 퇴사 2, Time: 760 ms, Memory: 345520 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n+2][2];
+        int[] dp = new int[n+2];
+        for (int i = 1; i<n+1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+        
+        int max = -1;
+        for (int i = 1; i<=n+1; i++) {
+            if (max<dp[i]) max = dp[i];
+            int next = i+arr[i][0];
+            int cost = arr[i][1];
+            if (next < n+2) {
+                dp[next] = Math.max(dp[next], max+cost);
+            }
+        }
+
+        System.out.println(dp[n+1]);
+
+    }
+}
