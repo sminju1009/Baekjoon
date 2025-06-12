9d536bfdb09d2445a0313f0930337ed5087e66da
서민주 <sminju1009@gmail.com>
2025-06-12 09:48:13 +0900

    [Silver I] Title: IOIOI, Time: 248 ms, Memory: 20052 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,23 +1,27 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        String ioioi = "I";
-        for (int i = 0; i<n; i++) {
-            ioioi += "OI";
-        }
         int strlen = Integer.parseInt(br.readLine());
         String str = br.readLine();
-        int ilen = 2*n+1;
+
         int count = 0;
-        for (int i = 0; i<strlen-ilen+1; i++) {
-            if (str.substring(i, i+ilen).equals(ioioi)) {
+        int result = 0;
+        for (int i = 1; i<strlen-1;) {
+            if (str.charAt(i-1)=='I' && str.charAt(i)=='O' && str.charAt(i+1)=='I') {
                 count++;
+                if (count==n) {
+                    result++;
+                    count--;
+                }
+                i+=2;
+            } else {
+                count = 0;
+                i++;
             }
         }
-        System.out.println(count);
+        System.out.println(result);
     }
 }
