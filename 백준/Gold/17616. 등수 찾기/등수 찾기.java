63c948e5a954006872621202feb083bfc0534cc7
서민주 <sminju1009@gmail.com>
2025-06-23 09:45:23 +0900

    [Gold III] Title: 등수 찾기, Time: 692 ms, Memory: 152700 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/17616. 등수 찾기/등수 찾기.java
+++ 백준/Gold/17616. 등수 찾기/등수 찾기.java
@@ -3,8 +3,8 @@ import java.io.*;
 
 public class Main {
     static int n;
-    static ArrayList<ArrayList<Integer>> sToE;
-    static ArrayList<ArrayList<Integer>> eToS;
+    static ArrayList<ArrayList<Integer>> higher; 
+    static ArrayList<ArrayList<Integer>> lower;  
     static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
@@ -13,49 +13,44 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int x = Integer.parseInt(st.nextToken()) - 1;
-        sToE = new ArrayList<>();
-        eToS = new ArrayList<>();
-        for (int i = 0; i<n; i++) {
-            sToE.add(new ArrayList<>());
-            eToS.add(new ArrayList<>());
+        
+        higher = new ArrayList<>();
+        lower = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            higher.add(new ArrayList<>());
+            lower.add(new ArrayList<>());
         }
-        for (int i = 0; i<m; i++) {
+        
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken()) - 1;
             int b = Integer.parseInt(st.nextToken()) - 1;
-            sToE.get(a).add(b);
-            eToS.get(b).add(a);
+            lower.get(a).add(b);   
+            higher.get(b).add(a); 
         }
 
         visited = new boolean[n];
-        dfs(0, x);
-        int answer1 = 0;
-        for (int i = 0; i<n; i++) {
-            if (visited[i] && i!=x) answer1++;
-        }
+        int lowerCount = dfsCount(x, lower) - 1; 
+        
         visited = new boolean[n];
-        dfs(1, x);
-        int answer2 = 0;
-        for (int i = 0; i<n; i++) {
-            if (visited[i] && i!=x) answer2++;
-        }
-        System.out.println((n-answer1) + " " + (answer2+1));
+        int higherCount = dfsCount(x, higher) - 1; 
+        
+        int bestRank = higherCount + 1;
+        int worstRank = n - lowerCount;
+        
+        System.out.println(bestRank + " " + worstRank);
     }
 
-    public static void dfs(int param, int num) {
-        visited[num] = true;
-        if (param==0) {
-            for (int i: sToE.get(num)) {
-                if (!visited[i]) {
-                    dfs(param, i);
-                }
-            }
-        } else if (param==1) {
-            for (int i: eToS.get(num)) {
-                if (!visited[i]) {
-                    dfs(param, i);
-                }
+    public static int dfsCount(int node, ArrayList<ArrayList<Integer>> graph) {
+        visited[node] = true;
+        int count = 1;
+        
+        for (int next : graph.get(node)) {
+            if (!visited[next]) {
+                count += dfsCount(next, graph);
             }
         }
+        
+        return count;
     }
 }
