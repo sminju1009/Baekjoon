ebc2dbf0e35dc4c47ced0d81177b2ae02ce1692f
서민주 <sminju1009@gmail.com>
2025-09-04 08:52:13 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 1472 ms, Memory: 101012 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n, m;
+    static int[] visited;
+    static ArrayList<ArrayList<Integer>> arr;
+    static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        visited = new int[n+1];
+        arr = new ArrayList<>();
+        for (int i = 0; i<=n; i++) {
+            arr.add(new ArrayList<>());
+        }
+        for (int i = 0; i<m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            arr.get(s).add(e);
+            arr.get(e).add(s);
+        }
+
+        for (int i = 0; i<arr.size(); i++) {
+            Collections.sort(arr.get(i), Collections.reverseOrder());
+        }
+
+        count = 1;
+        visited[r] = count;
+        dfs(r);
+
+        for (int i = 1; i<=n; i++) {
+            System.out.println(visited[i]);
+        }
+    }
+
+    public static void dfs(int now) {
+        for (int i: arr.get(now)) {
+            if (visited[i]==0) {
+                count++;
+                visited[i] = count;
+                dfs(i);
+            }
+        }
+    }
+}
