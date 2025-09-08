0250c6540191be168d396ed114b055de69b9d105
서민주 <sminju1009@gmail.com>
2025-09-08 09:14:59 +0900

    [Silver I] Title: 동물원, Time: 104 ms, Memory: 14496 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 3;
+        for (int i = 2; i<=n; i++) {
+            dp[i] = (dp[i-1]*2 + dp[i-2]) % 9901;
+        }
+        System.out.println(dp[n]);
+    }
+}
