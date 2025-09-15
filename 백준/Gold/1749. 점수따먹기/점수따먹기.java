27f023b6093ae8e7958335a76c3093a2f9100f19
서민주 <sminju1009@gmail.com>
2025-09-15 10:11:03 +0900

    [Gold IV] Title: 점수따먹기, Time: 1348 ms, Memory: 20596 KB -BaekjoonHub

---
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][m];
+        
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        int[][] prefixSum = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                prefixSum[i][j] = arr[i-1][j-1] + prefixSum[i-1][j] + prefixSum[i][j-1] - prefixSum[i-1][j-1];
+            }
+        }
+        
+        int max = Integer.MIN_VALUE;
+        
+        for (int r1 = 0; r1 < n; r1++) {
+            for (int c1 = 0; c1 < m; c1++) {
+                for (int r2 = r1; r2 < n; r2++) {
+                    for (int c2 = c1; c2 < m; c2++) {
+                        int sum = prefixSum[r2+1][c2+1] - prefixSum[r1][c2+1] - prefixSum[r2+1][c1] + prefixSum[r1][c1];
+                        max = Math.max(max, sum);
+                    }
+                }
+            }
+        }
+        
+        System.out.println(max);
+    }
+}
